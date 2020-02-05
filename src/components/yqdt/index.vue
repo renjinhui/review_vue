1a132c6f262e9ee1c3489f508c14bee344cc35cc
renjinhui <1227760869@qq.com>
2020-02-05 15:15:10 +0800

    'm'

--- src/components/yqdt/index.vue
+++ src/components/yqdt/index.vue
@@ -12,11 +12,16 @@
             <el-button>免费问医生</el-button>
         </div>
         疫情动态
+        <div>
+            <mymap></mymap>
+        </div>
     </div>
 </template>
 <script>
 // @ is an alias to /src
 import china_domestic from './china_domestic.vue'
+import mymap from './map'
+import 'echarts/map/js/china.js'
 export default {
     name: 'yqdt',
     data() {
@@ -25,7 +30,7 @@ export default {
         }
     },
     components: {
-        china_domestic
+        china_domestic,mymap
     }
 }
 </script>
@@ -34,11 +39,14 @@ export default {
     padding: 20px;
 }
     .btn_box{
-        overflow: hidden;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
         button{
             display: block;
             width: 300px;
             margin: 10px 20px;
+            margin-left: 0;
             float: left;
         }
     }
