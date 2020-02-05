103b585bbe0336eb286ac3932e034304c8ea268a
renjinhui <1227760869@qq.com>
2020-02-05 10:23:25 +0800

    第三次

---
+++ src/views/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div id="nav">
+      <router-link to="/yqdt">疫情动态</router-link> |
+      <router-link to="/qxmap">迁徙地图</router-link> |
+      <router-link to="/hotsearch">全民热搜</router-link> |
+      <router-link to="/news">实时直播</router-link>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+export default {
+  name: "INDEX",
+  data() {
+    return {};
+  },
+  components: {}
+};
+</script>
+<style lang="less">
+</style>
