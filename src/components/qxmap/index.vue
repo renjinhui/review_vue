6821cc349ad526b752bf4013c508aca43e541940
renjinhui <1227760869@qq.com>
2020-02-11 11:49:24 +0800

    'm'

--- src/components/qxmap/index.vue
+++ src/components/qxmap/index.vue
@@ -1,36 +1,44 @@
 <template>
     <div class='qxmapBox'>
         <h1>全国迁徙热门城市</h1>
-        <pre><div class='timeBox'>2020.2.9更新  来自百度地图</div></pre>
+        <pre><div class='timeBox'>{{time}}更新  来自百度地图</div></pre>
         <switchtab 
             :list="['热门迁入地','热门迁出地']" 
             @change='change'></switchtab>
-        迁徙地图
+        <mylist :data='list' ></mylist>
     </div>
 </template>
 <script>
 // @ is an alias to /src
 import { moveInOut } from "@/api";
 import switchtab from './switchTab.vue'
+import mylist from './list.vue'
 export default {
     name: 'qxmap',
     data() {
         return {
-        
+            moveInList:[],
+            moveOutList:[],
+            list:[],
+            time:''
         }
     },
     created() {
         moveInOut().then(data=>{
             console.log(data)
+            this.list = this.moveInList = data.result.moveInList || [];
+            this.moveOutList = data.result.moveOutList;
+            this.time = data.result.time.replace(/(\d{4})(\d{2})(\d{2})/,'$1年$2月$3日')
         })
     },
     methods: {
         change(index,item){
-            console.log(index,item)
+            index ? this.list = this.moveOutList : this.list = this.moveInList
+            this.list.myIndex = index;
         }
     },
     components: {
-        switchtab
+        switchtab,mylist
     }
 }
 </script>
---
+++ src/components/qxmap/list.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="list_box">
+        <ul class="til">
+          <li class="sort">排名</li>
+          <li class="name">城市名称</li>
+          <li class="percent">{{data.myIndex ? '迁出' : '迁入'}}比例</li>
+        </ul>
+        <ul class="body">
+          <li v-for='(item,index) in data' @click='go(item)'>
+            <div class="sort">{{index+1}}</div>
+            <div class="name">{{item.city_name}} <span>({{item.province_name}})</span></div>
+            <div class="percent">{{item.value}}%</div>
+          </li>
+        </ul>
+    </div>
+</template>
+<script>
+// @ is an alias to /src
+export default {
+    name: 'mylist',
+    props:['data'],
+    data() {
+        return {
+        
+        }
+    },
+    methods: {
+      go(item){
+        window.open('https://qianxi.baidu.com/?from=shoubai#city='+item.city_code)
+      }
+    },
+    components: {
+        
+    }
+}
+</script>
+<style lang="less" scoped>
+  .list_box{
+    width: 650px;
+    margin: 20px auto;
+    ul{
+      list-style: none;
+    }
+    .til,.body li{
+      display: flex;
+      height: 50px;
+      line-height: 50px;
+      border-bottom: 1px solid #d5dbdf;
+      .sort,.percent{
+        width: 80px;
+      }
+      .name{
+        flex: auto
+      }
+    }
+    
+  }
+</style>
